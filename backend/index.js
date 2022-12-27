c521d1aa5ee5bf2ad864226192f4603c68bd215b
Moundir Jabir <elmondhirjabir@gmail.com>
2022-12-27 11:30:10 +0100

    POR-5 structure projet backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const expressValidator = require('express-validator')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true
+}).then(() => console.log('database connected'))
+  .catch((e) => console.log('not connect to database', e))
+
+//Routers
+const authRouter = require('./routes/auth')
+
+app.use(express.json())
+app.use(expressValidator())
+app.use(cookieParser())
+app.use(cors())
+
+app.use('/api/auth', authRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`app running on port ${port}`)
+})
